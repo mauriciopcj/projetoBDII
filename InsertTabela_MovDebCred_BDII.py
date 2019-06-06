f12b340b2e5afefb71b82f4b88089bee216603c4
Mauricio Pereira <mauriciojuniordesigner@gmail.com>
2019-06-06 20:44:36 -0300

    corrigindo testes

--- InsertTabela_MovDebCred_BDII.py
+++ InsertTabela_MovDebCred_BDII.py
@@ -18,19 +18,13 @@ def verificar_digito(conta):
 
 #--------------
 # contas analiticas
-"""
+
 vetor = ['11010100000','11010200000','11020100000','11020200000','11020300000','11020400000',
          '11030100000','11030200000','11030300000','12010000000','12020000000','12030000000']
-"""
 
-vetor = ['10000000000', '11000000000', '11010000000', '11010100000', '11010200000', '11020000000', 
-        '11020100000', '11020200000', '11020300000', '11020400000', '11030000000', '11030100000', 
-        '11030200000', '11030300000', '12000000000', '12010000000', '12020000000', '12030000000']
-for i in vetor:
-    print("'{}', '{}'".format(i,verificar_digito(i)))
 #---------------
 # loop maravilha
-"""
+
 for i in range(40):
     conta = random.randrange(0,12)      # escolhe uma conta do vetor
     dia = random.randrange(1, 29)       # está ate 28 para não dar erro em fevereiro
@@ -40,4 +34,3 @@ for i in range(40):
     valor = random.randrange(20, 401)
     print("INSERT INTO MovDebCred (numConta, dig, data, debCred, valor) VALUES ({}, '{}', '{}-{}-{}', '{}', {});"
           .format(vetor[conta], verificar_digito(vetor[conta]), dia, mes, ano, debcred[random.randrange(0,2)], valor))
-"""
