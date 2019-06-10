ce6313fcfff230b860379355a75a761e2317f000
Renato César <rencesarlira@gmail.com>
2019-06-10 15:58:14 -0300

    Refactoring

--- utils_functions.sql
+++ utils_functions.sql
@@ -9,15 +9,13 @@ AS $$
 	DECLARE
 		mask VARCHAR := 'X.X.XX.XX.XXX.XX';
 		formated_value VARCHAR := '';
-		account_number_char VARCHAR;
 		interator integer := 1;
 	BEGIN
-		account_number_char = CAST(account_number AS VARCHAR);
 		FOR i IN 1..16 LOOP
 			IF substring(mask from i for 1) = 'X' THEN
 				formated_value = CONCAT(
 					formated_value,
-					substring(account_number_char from interator for 1)
+					substring(account_number from interator for 1)
 				);
 				interator = interator + 1;
 			ELSE
